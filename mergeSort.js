3f181f2a735cc94197650d0944486f3e2d7ed04f
Hermshy Mac <94696321+hoomanhorsey@users.noreply.github.com>
2024-09-27 11:29:41 +1000

    MergeSort final

--- mergeSort.js
+++ mergeSort.js
@@ -1,160 +1,157 @@
 function mergeSort(array) {
   if (array.length < 2) {
-    console.log("end of array");
-    console.log(array);
+    console.log("returning now!");
     return array;
   } else {
-    // if (array.length > 2) var halfLength = 0;
+    console.log("starting recursive function: array");
+    console.log(array);
 
-    // halves the array into 2 arrays.
     halfLength = Math.floor(array.length / 2);
     var leftArray = [];
     var rightArray = [];
 
     leftArray = array.slice(0, halfLength);
     rightArray = array.slice(halfLength, array.length);
-
-    console.log("halflength: " + halfLength);
-    console.log("arrays for sorting in temp temp");
 
     console.log("leftArray");
     console.log(leftArray);
     console.log("rightArray");
     console.log(rightArray);
 
-    mergeSort(leftArray);
-    mergeSort(rightArray);
+    var first = [];
+    first = first.concat(mergeSort(leftArray));
 
-    var temptempArray = [];
-    var i = 0;
-    var j = 0;
-    var k = 0;
-    console.log(array.length);
+    var second = [];
+    second = second.concat(mergeSort(rightArray));
 
-    //note the arrays aren't sorted so they aren't being merged propery
-    while (i < leftArray.length && j < rightArray.length) {
-      if (leftArray[i] < rightArray[j]) {
-        temptempArray[k] = leftArray[i];
-        console.log("i: " + i + ", j " + j + ", k: " + k);
+    console.log("first array - " + first);
+    console.log("second array - " + second);
+    // var tempArray = [];
+    // if (first < second) {
+    //   console.log("first is smaller");
+    //   tempArray = tempArray.concat(first);
+    //   tempArray = tempArray.concat(second);
+    // } else {
+    //   console.log("second is smaller");
+    //   tempArray = tempArray.concat(second);
+    //   tempArray = tempArray.concat(first);
+    // }
+    let i = 0,
+      j = 0,
+      k = 0;
+    var tempArray = [];
 
-        console.log(temptempArray);
+    while (i < first.length || j < second.length) {
+      console.log("merging");
+
+      if (first[i] === undefined) {
+        tempArray[k] = second[j];
+        j++;
+        k++;
+        console.log("First was undefined! Yeah");
+        console.log(
+          "i: " +
+            i +
+            ", j " +
+            j +
+            ", k: " +
+            k +
+            ", first length: " +
+            first.length +
+            ", second length: " +
+            second.length
+        );
+      } else if (second[j] === undefined) {
+        tempArray[k] = first[i];
         i++;
         k++;
-      } else {
-        temptempArray[k] = rightArray[j];
-        console.log("i: " + i + ", j " + j + ", k: " + k);
+        console.log("Second was undefined! Yeah");
+        console.log(
+          "i: " +
+            i +
+            ", j " +
+            j +
+            ", k: " +
+            k +
+            ", first length: " +
+            first.length +
+            ", second length: " +
+            second.length
+        );
+      } else if (first[i] < second[j]) {
+        console.log(first[i] + " and " + second[j]);
 
-        console.log(temptempArray);
+        tempArray[k] = first[i];
+        console.log("first smaller - value of element: " + first[i]);
+
+        i++;
+        k++;
+        console.log(
+          "i: " +
+            i +
+            ", j " +
+            j +
+            ", k: " +
+            k +
+            ", first length: " +
+            first.length +
+            ", second length" +
+            second.length
+        );
+      } else {
+        console.log(first[i] + " and " + second[j]);
+
+        tempArray[k] = second[j];
+        console.log("second smaller - value of element: " + second[j]);
 
         j++;
         k++;
+        console.log(
+          "i: " +
+            i +
+            ", j " +
+            j +
+            ", k: " +
+            k +
+            ", first length: " +
+            first.length +
+            ", second length: " +
+            second.length
+        );
       }
+      console.log("tempArray");
+      console.log(tempArray);
     }
 
-    for (; i < leftArray.length; i++) {
-      temptempArray[k] = leftArray[i];
-    }
-    for (; j < leftArray.length; j++) {
-      temptempArray[k] = leftArray[i];
-    }
-    console.log("temptempArray");
-    console.log(temptempArray);
-
-    return temptempArray;
+    return tempArray;
   }
-
-  // if (leftArray.length < 2) {
-  //   console.log("calling right");
-  //   return mergeSort(rightArray);
-  // } else {
-  //   console.log("calling left");
-  //   return mergeSort(leftArray);
-
-  // mergeSort(rightArray);
-
-  // let tempArray = [];
-  // if (leftArray[0] < rightArray[0]) {
-  //   tempArray.push(leftArray[0]);
-  //   tempArray.push(rightArray[0]);
-  // } else {
-  //   tempArray.push(rightArray[0]);
-  //   tempArray.push(leftArray[0]);
-  // }
-  // mergeSort(leftArray);
-  // console.log(tempArray);
 }
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 9]));
 
-function smergeSort(array) {
-  if (array < 2) {
-    console.log("hit base case. Returning:");
-    console.log(array);
-    return array;
-  } else {
-    console.log("array entered into recursive bit");
-    console.log(array);
-    halfLength = Math.floor(array.length / 2);
-    var leftArray = [];
-    var rightArray = [];
-
-    leftArray = array.slice(0, halfLength);
-    rightArray = array.slice(halfLength, array.length);
-    console.log("leftArray and rightArray");
-    console.log(leftArray);
-    console.log(rightArray);
-
-    //merge
-    let temporaryArray = [];
-    for (let i = 0; i < halfLength; i++) {
-      var first = leftArray.shift();
-      var second = rightArray.shift();
-
-      console.log("first and second" + first + second);
-
-      if (first < second) {
-        temporaryArray.unshift(first);
-        first = leftArray.shift();
-      } else {
-        temporaryArray.unshift(second);
-        second = rightArray.shift();
-      }
-    }
-
-    console.log("smergeSort tempyarray");
-    console.log(temporaryArray);
-  }
-
-  return smergeSort(leftArray);
-}
+console.log(
+  mergeSort([
+    82, 17, 5, 32, 99, 7, 44, 25, 61, 38, 73, 49, 16, 2, 90, 57, 29, 86, 63, 41,
+    3, 94, 68, 20, 48, 10, 77, 14, 81, 22, 33, 50, 89, 1, 36, 72, 95, 60, 13,
+    79, 46, 9, 91, 26, 6, 55, 85, 31, 67, 100,
+  ])
+);
 
 console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
-// console.log(smergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
 
-// console.log(mergeSort([105, 79, 100, 110]));
-// console.log(mergeSort([15, 9, 10, 110, 98]));
+console.log(mergeSort([105, 79, 100, 110]));
 
-///pseduocode
-
-// identify length of array (array.length/2)
-
-// left half of array is array[(array.length/2)-1]
-// put contents into LeftArray
-// right half of array is array[array.length/2]
-// put contents into RightArray
-
-// check - if leftArray.length > 1
-//mergeSort(leftArray)
-
-// check -  if LeftArray.lenght < 2, if not
-// mergeSort(rightArray)
-
-// else if length = 1
-// create temp array[]
-// then compare left array with right array
-// if left array is smaller, then put left array item into new array first
-//    else, put right array item into new array
-
-// if Left Array.length is less than 2, then mergeSort(rightARray)
-
-// sort left half of array
-// sort right half of array
-// merge two together.
+// while (i < first.length && j < second.length) {
+//   if (first[i] < second[j]) {
+//     tempArray[k] = leftArray[i];
+//     console.log("i: " + i + ", j " + j + ", k: " + k);
+//     console.log(tempArray);
+//     i++;
+//     k++;
+//   } else {
+//     tempArray[k] = second[j];
+//     console.log("i: " + i + ", j " + j + ", k: " + k);
+//     console.log(tempArray);
+//     j++;
+//     k++;
+//   }
+// }
